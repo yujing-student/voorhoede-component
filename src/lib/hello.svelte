28fe2146b9aa034475a35f13fc564cb88676e3f8
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-11-13 20:22:02 +0100

    build:I make a component for the button `schrijf me in`

--- src/lib/hello.svelte
+++ src/lib/hello.svelte
@@ -72,6 +72,34 @@
         </label>
 
 
+
+
+        <div>
+            <a href="bedankt" class="button">Schrijf me in</a>  <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                    d="M8 3L13.0001 8.0001L8 13.0002"
+                    stroke="#222226"
+                    stroke-width="1.5"
+                    stroke-miterlimit="4.62023"
+            />
+            <path
+                    d="M3 8L13 8"
+                    stroke="#222226"
+                    stroke-width="1.5"
+                    stroke-miterlimit="4.62023"
+                    stroke-linejoin="round"
+            />
+        </svg>
+        </div>
+
+
+
     </fieldset>
 </form>
 
@@ -102,6 +130,23 @@
         width: 100%;
         border-bottom: 1.5px solid black;
     }
+    .button{
+        border-left: 1px solid #666666;
+        border-top: 1px solid #666666;
+        border-bottom: 1px solid #666666;
+        width: 6rem;
+        padding: 0.55rem;
+        margin-right: -0.3rem;
+        color: #666666;
+        text-decoration: none;
+    }
+    svg{
+        border: 1px solid #666666;
+        padding: 0.6rem;
+        margin-bottom: -0.8rem;
+    }
+
+
 
     @media (max-width: 700px) {
             .form-label {
@@ -114,13 +159,13 @@
             }
         }
 
-    @media (min-width: 700px) {
+    @media (min-width: 800px) {
         fieldset {
             display: grid;
                     grid-template-columns: auto auto;
                     /*background-color: red;*/
             max-width: 700px;
-            column-gap: 1rem;
+            gap: 1rem;
         }
     }
 
